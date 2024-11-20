ec36e13665fde6f706f652160c1bd39990b72a2c
kimjuneseo <june3929213512@gmail.com>
2024-11-20 23:57:26 +0900

    feat: 진행중..

--- PCCP_2번_퍼즐게임.js
+++ PCCP_2번_퍼즐게임.js
@@ -1,53 +1,62 @@
 /**
- * 
+ * https://school.programmers.co.kr/learn/courses/30/lessons/340212?language=javascript
  * @param {number[]} diffs 난이도
  * @param {string[]} times 소요시간
  * @param {string} limit 이전 퍼즐 소요시간
  * @returns 
  */
+
 function solution(diffs, times, limit) {
-    // 레벨을 1부터 다 올린다?
-    // 더 좋은 방법 없을까 근사치
-    // 범위를 잡아서 하는게 덜 계산하지 않을까?
-    let level = 1;
-    // function 
-    // result는 level = tiems
+
     function calcLowLevel({ prevTime, currentTime, currentDiff }) {
         const returnTime = ((currentDiff - level) * (prevTime + currentTime)) + currentTime;
 
         return returnTime;
     }
     function minLevel() {
-        times.reduce((prevLimitTime, currentTime, index) => {
-            const currentDiff = diffs[index];
-            const plusTime = currentDiff <= level ? currentDiff : calcLowLevel({ prevTime: times[index - 1], currentDiff, currentTime });
-            return prevLimitTime + plusTime
-        }, 0);
+        const maxDiff = diffs.sort((a, b) => b - a);
+        console.log(maxDiff)
+        const sumTime = times.reduce((prev, current) => prev + current, 0);
+        // times.reduce((prevLimitTime, currentTime, index) => {
+        //     const currentDiff = diffs[index];
+        //     const plusTime = currentDiff <= level ? currentDiff : calcLowLevel({ prevTime: times[index - 1], currentDiff, currentTime });
+        //     return prevLimitTime + plusTime
+        // }, 0);
     }
+    minLevel();
 
-    let isEqualLimit = false;
-    while (!isEqualLimit) {
-        const currentLimit = times.reduce((prevLimitTime, currentTime, index) => {
-            const currentDiff = diffs[index];
-            const plusTime = currentDiff <= level ? currentDiff : calcLowLevel({ prevTime: times[index - 1], currentDiff, currentTime });
-            return prevLimitTime + plusTime
-        }, 0);
+    // let isEqualLimit = false;
+    // let level = 1;
+    // for (let level = 1; isEqualLimit; level++) {
+    //     const currentLimit = times.reduce((prevLimitTime, currentTime, index) => {
+    //         const currentDiff = diffs[index];
+    //         const plusTime = currentDiff < level ? currentDiff : calcLowLevel({ prevTime: times[index - 1], currentDiff, currentTime });
+    //         return prevLimitTime + plusTime
+    //     }, 0);
 
-        2 +
-            (diff - level * 6) + 4
-                (diff - level * 11) + 7
-        // 1번째 times보다는 클거고
-        // tiems 다 더했을때 limit보다 
-        console.log(currentLimit)
-        if (currentLimit <= limit) {
-            isEqualLimit = true;
-        }
-        else {
-            level++;
-        }
-    }
+    //     console.log(currentLimit);
+    //     if (currentLimit <= limit) {
+    //         isEqualLimit = true;
+    //     }
+    //     else {
+    //         level++;
+    //     }
 
-    return level;
+    // }
+
+    // return level;
 }
 
-console.log(solution([1, 99999, 100000, 99995], [9999, 9001, 9999, 9001], 3456789012));
+// time으로 최소 리밋을 구해보자
+// 13 diffs에 최대 값으로 계산을 해본다
+
+//1.  지금은 calcLowLevel가 계속 돌아가지만 미리 n번 틀렸을때 흐르는 시간을 가지고 있으면? x
+
+
+// 2 +
+//     (diff - level * 6) + 4
+//         (diff - level * 11) + 7
+// 1번째 times보다는 클거고
+// tiems 다 더했을때 limit보다 
+// console.log(currentLimit)
+console.log(solution([1, 328, 467, 209, 54], [2, 7, 1, 4, 3], 1723));
